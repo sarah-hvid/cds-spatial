cf49868dc636858728e635fcfa5d9ac3fb7bfedf
Sarah Hvid Andersen <shandersen@hotmail.com>
2022-02-10 13:05:50 +0100

    adding my files

---
+++ Week01/01_LeafletBackgrounds_SHA.R
@@ -0,0 +1,216 @@
+###   GETTING STARTED WITH LEAFLET
+
+
+## Choose favorite backgrounds in:
+# https://leaflet-extras.github.io/leaflet-providers/preview/
+## beware that some need extra options specified
+
+# Packages
+install.packages("leaflet")
+install.packages("htmlwidget")
+
+# Example with Markers
+library(leaflet)
+library(htmlwidgets)
+
+popup = c("Robin", "Jakub", "Jannes")
+
+leaflet() %>%
+  addProviderTiles("Esri.WorldPhysical") %>% 
+  #addProviderTiles("Esri.WorldImagery") %>% 
+  addAwesomeMarkers(lng = c(-3, 23, 11),
+                    lat = c(52, 53, 49), 
+                    popup = popup)
+
+
+## Sydney with setView
+leaflet() %>%
+  addTiles() %>%
+  addProviderTiles("Esri.WorldImagery", 
+                   options = providerTileOptions(opacity=0.5)) %>% 
+  setView(lng = 151.005006, lat = -33.9767231, zoom = 10)
+
+
+leaflet() %>%
+  addTiles() %>%
+  addProviderTiles("Esri.WorldImagery", 
+                   options = providerTileOptions(opacity=0.5)) %>% 
+  setView(lng = 176.005006, lat = -40.9767231, zoom = 6)
+
+
+# Europe with Layers
+leaflet() %>% 
+  addTiles() %>% 
+  setView( lng = 2.34, lat = 48.85, zoom = 5 ) %>% 
+  addProviderTiles("Esri.WorldPhysical", group = "Physical") %>% 
+  addProviderTiles("Esri.WorldImagery", group = "Aerial") %>% 
+  addProviderTiles("MtbMap", group = "Geo") %>% 
+  
+  addLayersControl(
+    baseGroups = c("Geo","Aerial", "Physical"),
+    options = layersControlOptions(collapsed = T))
+
+# note that you can feed plain Lat Long columns into Leaflet
+# without having to convert into spatial objects (sf), or projecting
+
+
+########################## SYDNEY HARBOUR DISPLAY WITH LAYERS
+
+# Set the location and zoom level
+leaflet() %>% 
+  setView(151.2339084, -33.85089, zoom = 13) %>%
+  addTiles()  # checking I am in the right area
+
+
+# Bring in a choice of esri background layers  
+
+l_aus <- leaflet() %>%   # assign the base location to an object
+  setView(151.2339084, -33.85089, zoom = 13)
+
+esri <- grep("^Esri", providers, value = TRUE)
+
+for (provider in esri) {
+  l_aus <- l_aus %>% addProviderTiles(provider, group = provider)
+}
+
+AUSmap <- l_aus %>%
+  addLayersControl(baseGroups = names(esri),
+                   options = layersControlOptions(collapsed = FALSE)) %>%
+  addMiniMap(tiles = esri[[1]], toggleDisplay = TRUE,
+             position = "bottomright") %>%
+  addMeasure(
+    position = "bottomleft",
+    primaryLengthUnit = "meters",
+    primaryAreaUnit = "sqmeters",
+    activeColor = "#3D535D",
+    completedColor = "#7D4479") %>% 
+  htmlwidgets::onRender("
+                        function(el, x) {
+                        var myMap = this;
+                        myMap.on('baselayerchange',
+                        function (e) {
+                        myMap.minimap.changeLayer(L.tileLayer.provider(e.name));
+                        })
+                        }") %>% 
+  addControl("", position = "topright")
+
+AUSmap
+################################## SAVE FINAL PRODUCT
+
+# Save map as a html document (optional, replacement of pushing the export button)
+# only works in root
+
+saveWidget(AUSmap, "AUSmap.html", selfcontained = TRUE)
+#########################################################
+#
+# Task 1: Create a Danish equivalent with esri layers, call it DKmap
+
+
+
+
+
+
+# Set the location and zoom level
+leaflet() %>% 
+  setView(11, 56.2, zoom = 6.5) %>%
+  addTiles()  # checking I am in the right area
+
+
+# Bring in a choice of esri background layers  
+
+l_dk <- leaflet() %>%   # assign the base location to an object
+  setView(11, 56.2, zoom = 6.5)
+
+esri <- grep("^Esri", providers, value = TRUE)
+
+for (provider in esri) {
+  l_dk <- l_dk %>% addProviderTiles(provider, group = provider)
+}
+
+
+DKmap <- l_dk %>%
+  addLayersControl(baseGroups = names(esri),
+                   options = layersControlOptions(collapsed = FALSE)) %>%
+  addMiniMap(tiles = esri[[1]], toggleDisplay = TRUE,
+             position = "bottomright") %>%
+  addMeasure(
+    position = "bottomleft",
+    primaryLengthUnit = "meters",
+    primaryAreaUnit = "sqmeters",
+    activeColor = "#3D535D",
+    completedColor = "#7D4479") %>% 
+  htmlwidgets::onRender("
+                        function(el, x) {
+                        var myMap = this;
+                        myMap.on('baselayerchange',
+                        function (e) {
+                        myMap.minimap.changeLayer(L.tileLayer.provider(e.name));
+                        })
+                        }") %>% 
+  addControl("", position = "topright")
+
+DKmap
+################################## SAVE FINAL PRODUCT
+
+# Save map as a html document (optional, replacement of pushing the export button)
+# only works in root
+
+saveWidget(DKmap, "DKmap.html", selfcontained = TRUE)
+
+
+
+
+
+
+
+
+
+# Task 2: Start collecting spatial data into a spreadsheet: https://docs.google.com/spreadsheets/d/1PlxsPElZML8LZKyXbqdAYeQCDIvDps2McZx1cTVWSzI/edit#gid=1817942479
+#
+
+# inserted three locations from Als
+
+#
+################################## ADD DATA TO LEAFLET
+# Libraries
+library(tidyverse)
+library(googlesheets4)
+library(leaflet)
+
+places <- read_sheet("https://docs.google.com/spreadsheets/d/1PlxsPElZML8LZKyXbqdAYeQCDIvDps2McZx1cTVWSzI/edit#gid=1817942479",
+                     range = "SA2022", col_types = "cccnncnc")
+
+gs4_deauth()
+
+
+glimpse(places)
+
+leaflet() %>% 
+  addTiles() %>% 
+  addMarkers(lng = places$Longitude, 
+             lat = places$Latitude,
+             popup = places$Description)
+
+#########################################################
+#
+# Task 3: Read in the googlesheet data you and your colleagues populated with data and display it over your DKmap . 
+# The googlesheet is at https://docs.google.com/spreadsheets/d/1PlxsPElZML8LZKyXbqdAYeQCDIvDps2McZx1cTVWSzI/edit#gid=1817942479
+
+
+
+DKmap %>% 
+  addMarkers(lng = places$Longitude, 
+             lat = places$Latitude,
+             popup = paste(
+               "<strong>Placename: </strong>",places$Placename,
+               "<br> Type:",places$Type,
+               "<br><strong>Description: </strong>",places$Description,
+               "<br><strong>Stars: </strong>",places$Stars1_5,
+               "<br><strong>Notes: </strong>",places$Notes),
+             clusterOptions = markerClusterOptions())
+
+
+
+
+#########################################################
+
